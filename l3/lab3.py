5b4cce3e27b7e78502c8d844a2f9215656690203
krab0vsky <koldasa2005@mail.ru>
2024-11-21 20:54:30 +0400

    Update lab3.py

--- l3/lab3.py
+++ l3/lab3.py
@@ -13,34 +13,43 @@ inter = {
     '7': 'семь',
 }
 
+
 def valid(word):
     return len(word) > 0 and word[0] == '3' and all('0' <= ch <= '7' for ch in word) and int(word, 8) % 2 == 1
+
 
 def transform_number(num):
     dc = {}
     trans = []
+    ch = 0
     for d in num:
         if d in dc and d in inter:
             trans.append(inter[d])
+            ch += 1
         else:
             trans.append(d)
             dc[d] = 1
+    if ch > 0:
+        return ''.join(trans)
+    else:
+        return ''.join('')
 
-    return ''.join(trans)
 
 def proc(path):
     res = []
     with open(path, 'r', encoding='utf-8') as file:
         for line in file:
-            words = line.split() 
-            trans_line = [] 
+            words = line.split()
+            trans_line = []
             for word in words:
                 if valid(word):
                     trans_line.append(transform_number(word))
                 else:
-                    trans_line.append(word)
+                    continue
+
             res.append(' '.join(trans_line))
     for r in res:
         print(r)
+
 
 proc('inp.txt')
