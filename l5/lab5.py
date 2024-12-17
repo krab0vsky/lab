19ae6fa051ef1f1add03a233fd7ec54bd854c484
krab0vsky <koldasa2005@mail.ru>
2024-12-17 11:39:57 +0400

    Update lab5.py

--- l5/lab5.py
+++ l5/lab5.py
@@ -20,7 +20,8 @@ def recursion(n):
     if n == 0 or n == 1:
         return 1
     else:
-        return (-1)**n * (recursion(n - 1) / (factorial(2 * n) * recursion(n - 2) + 2))
+        min = 1 if n % 2 == 0 else -1
+        return min * (recursion(n - 1) / (factorial(2 * n) * recursion(n - 2) + 2))
 
 def iteration(n):
     if n == 0 or n == 1:
@@ -28,9 +29,14 @@ def iteration(n):
     else:
         prev1 = 1
         prev2 = 1  
-        last_value = 1  
+        last_value = 1
+        factorial = 2 
         for i in range(2, n + 1):
-            last_value = (-1)**i * (prev1 / (math.factorial(2 * i) * prev2 + 2))
+            f1 = 2*i-1
+            f2 = 2 * i
+            factorial *= f1 * f2
+            min = 1 if i % 2 == 0 else -1
+            last_value = min * (prev1 / (factorial * prev2 + 2))
             prev2 = prev1
             prev1 = last_value
         return last_value
@@ -42,4 +48,4 @@ iteration_time = timeit.timeit(lambda: iteration(n), number=1000)
 print(f"Результат рекурсии для n={n}: {recursion(n)}")
 print(f"Результат итерации для n={n}: {iteration(n)}")
 print(f"Время выполнения рекурсии: {recursion_time:.6f} секунд")
-print(f"Время выполнения итерации: {iteration_time:.6f} секунд")
+print(f"Время выполнения итерации: {iteration_time:.6f} секунд") 
